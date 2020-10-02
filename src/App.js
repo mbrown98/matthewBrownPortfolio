eb97cf5ef74f27adab62c8559ab46ccaaece34d6
Matt Brown <matthewbrown0798@gmail.com>
2020-10-02 15:44:28 -0400

    serverless test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Serverless</h1>
+        <h1>Sup peeps, this is gonna be website</h1>
+        <h3>Stay Tuned</h3>
       </header>
     </div>
   );
